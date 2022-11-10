cea88b035fe30ac82e46baad41bddc87bf1656d6
ChrisMcElroy <csmcelroy@me.com>
2022-11-10 12:49:58 +0000

    Initial Commit

---
+++ ticker/tickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  tickerApp.swift
+//  ticker
+//
+//  Created by Chris McElroy on 11/10/22.
+//
+
+import SwiftUI
+
+@main
+struct tickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

652770c34457fd4c3d90fe55e624fd9a1990b3bf
ChrisMcElroy <csmcelroy@me.com>
2024-07-24 16:12:37 -0400

    significantly strengthened ability to hide apps

---
+++ ticker/AppTimers.swift
@@ -0,0 +1,57 @@
+//
+//  AppTimers.swift
+//  ticker
+//
+//  Created by 4 on '24.7.24.
+//
+
+import SwiftUI
+
+var spotifyEnabled: Bool = false
+var anyTimer: Bool = false
+var hidesRemaining: [pid_t: Int] = [:]
+var finderApp: NSRunningApplication? = nil
+//var hideTimer: Timer?
+
+func tryToHide(app: NSRunningApplication, launched: Bool = false) {
+    guard !appAllowed(app) else { return }
+    app.hide()
+    var repeats = 0
+    Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true, block: { t in
+        if repeats == 0 && launched { app.activate(options: .activateAllWindows); app.unhide() }
+//        print("hiding!", app.bundleIdentifier ?? "", app.isHidden ? "hidden" : "visible", app.isActive ? "active" : "")
+        guard !appAllowed(app) else { t.invalidate(); return }
+        if repeats == 7 && app.isActive { print("redoing"); app.activate(options: .activateAllWindows); app.unhide(); finderApp?.activate() }
+        app.hide()
+        repeats += 1
+        if repeats == 10 { t.invalidate() }
+    })
+}
+
+func appAllowed(_ app: NSRunningApplication) -> Bool {
+    let appID = app.bundleIdentifier
+    // for testing
+//    if appID == "com.apple.dt.Xcode" { return true }
+    // avoiding hide fighting
+    if appID == "com.apple.finder" || appID == "chris.ticker" {
+        if appID == "com.apple.finder" { finderApp = app }
+        return true
+    }
+    // spotify rule
+    if appID == "com.spotify.client" && spotifyEnabled { return true }
+    // other app rule
+    return anyTimer
+}
+
+//func startHideTimer() {
+//    hideTimer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true, block: { _ in
+//        if !hidesRemaining.isEmpty { print("hiding!") }
+//        for pid in hidesRemaining.keys {
+//            guard let app = NSRunningApplication(processIdentifier: pid) else { print("problem!"); return }
+//            if appAllowed(app) { hidesRemaining[pid] = nil }
+//            app.hide()
+//            hidesRemaining[pid, default: 1] -= 1
+//            if hidesRemaining[pid, default: 0] <= 0 { hidesRemaining[pid] = nil }
+//        }
+//    })
+//}
--- ticker/tickerApp.swift
+++ ticker/tickerApp.swift
@@ -58,8 +58,6 @@ struct tickerApp: App {
 var hideWindow = NSWindow(contentRect: NSRect(x: 0, y: 0, width: 100, height: 100), styleMask: [], backing: .buffered, defer: false)
 var currentScreen = NSRect(x: 0, y: 0, width: 1000, height: 1000)
 var wakeFromSleepFunc: (() -> Void)? = nil
-var spotifyEnabled: Bool = false
-var anyTimer: Bool = false
 
 func redrawWindows() {
     setBrightness()
@@ -195,24 +193,22 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate {
 //            guard let info = boi.userInfo?["NSWorkspaceApplicationKey"], let app = info as? NSRunningApplication else { return }
 //            print("just unhid!", app.bundleIdentifier)
 //        })
+        
+//        startHideTimer()
         notificationCenter.addObserver(forName: NSWorkspace.didActivateApplicationNotification, object: nil, queue: nil, using: { boi in
             guard let info = boi.userInfo?["NSWorkspaceApplicationKey"], let app = info as? NSRunningApplication else { return }
-//            print("just activited!", app.bundleIdentifier)
-            let appID = app.bundleIdentifier
-            if (!anyTimer && appID != "com.apple.finder" && appID != "chris.ticker") || (app.bundleIdentifier == "com.spotify.client" && !spotifyEnabled) {
-                var repeats = 0
-                Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true, block: { t in
-//                    print("hiding from active", app.isFinishedLaunching)
-                    repeats += 1
-                    if repeats > 10 { t.invalidate(); return }
-                    if app.isHidden {
-//                        print("hidden from active")
-                        t.invalidate()
-                        return
-                    }
-                    app.hide()
-                })
-            }
+//            print("just activited!", app.bundleIdentifier, app.isHidden)
+            tryToHide(app: app)
+        })
+        notificationCenter.addObserver(forName: NSWorkspace.willLaunchApplicationNotification, object: nil, queue: nil, using: { boi in
+            guard let info = boi.userInfo?["NSWorkspaceApplicationKey"], let app = info as? NSRunningApplication else { return }
+//            print("will launch!", app.bundleIdentifier, app.isHidden)
+            tryToHide(app: app)
+        })
+        notificationCenter.addObserver(forName: NSWorkspace.didLaunchApplicationNotification, object: nil, queue: nil, using: { boi in
+            guard let info = boi.userInfo?["NSWorkspaceApplicationKey"], let app = info as? NSRunningApplication else { return }
+//            print("just launched!", app.bundleIdentifier, app.isHidden)
+            tryToHide(app: app, launched: true)
         })
 		
 		return
